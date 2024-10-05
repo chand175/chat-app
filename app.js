a8e03507b18a23f7fdb7b1f80933da063db514ea
shaik chand basha <skchand175@gmail.comchand175chand175>
2024-10-06 02:12:00 +0530

    chat ai

--- app.js
+++ app.js
@@ -1,26 +1,73 @@
-let chatContainer = document.querySelector('.chat-container');
-let sendButton = document.getElementById('send-button');
-let messageInput = document.getElementById('message-input');
+
+ 
+const prompt = document.querySelector("#prompt");
+const button = document.querySelector('#btn');
+const chatContainer = document.querySelector('.chat-container')
+let userMessage = null;
+
+let apiUrl =  'https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=AIzaSyAgAF8UTQvoECJpz5YpN5sbnr7nd_shM10'
 
 
-function addMessage() {
+function createChat(html , className){
+    let div = document.createElement('div')
+    div.classList.add(className)
+    div.innerHTML=html
+    return div
+}
 
-    var messageElement = document.createElement('p');
-    messageElement.innerText = messageInput.value.trim();
+async function getApiResponse(chatBox) {
+    let para = chatBox.querySelector('.text')
+     try{
+        let response =  await fetch(apiUrl , {
+            method: "POST",
+            headers:{'Content-Type': 'application/json'},
+            body:JSON.stringify({
+                contents:[{"parts":[{"text":userMessage}]}]
+            })
+        })
 
-    chatContainer.appendChild(messageElement);
-    messageInput.value = "";
+        let data = await response.json();
+
+        let apiResponse = data?.candidates[0].content.parts[0].text ;
+      para.innerText = apiResponse
+
+
+     }
+     catch(err){
+        console.log(`This is the error : ${err}`)
+
+     }
+    
+}
+
+function showAnswer(){
+    let html = `<div class="chat-box">
+            <div class="img"> 
+                <img src="./images/chatBox.webp" alt="">
+            </div>
+            <p class="text"></p>
+        </div> `
+        let chatBox = createChat(html , "chat-box")
+        chatContainer.appendChild(chatBox)
+getApiResponse(chatBox)
 }
 
 
-sendButton.addEventListener('click', (e) => {
+button.addEventListener("click",(e)=>{
     e.preventDefault()
-    if (messageInput.value.trim() != "") {
-        addMessage();
-    }
+   userMessage= prompt.value
+  if(!userMessage) return;
+  let html = ` <div class="img"> 
+                <img src="./images/user.png" alt="">
+            </div>
+            <p class="text"> </p>`;
+            let userChat= createChat(html ,"user-chat")
+            userChat.querySelector('.text').innerText = userMessage
+            chatContainer.appendChild(userChat)
+            prompt.value = ""
+
+            setTimeout(showAnswer,500)
 })
-
-
 
 
 
